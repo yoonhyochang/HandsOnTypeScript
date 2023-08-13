e613f053f254908f41de847ceab8dfee87f36138
gptus <yhc9308@naver.com>
2023-08-13 15:14:33 +0900

    인터섹션 타입과 유니온 타입

--- Chap2/intersection.ts
+++ Chap2/intersection.ts
@@ -1,5 +1,6 @@
+// obj는 name 프로퍼티 (문자열 타입)와 age 프로퍼티 (숫자 타입)를 모두 가져야 하는 객체
 let obj: { name: string } & { age: number } = {
-    name: 'tom',
-    age: 25
+    name: 'tom', // 이름 설정
+    age: 25      // 나이 설정
 }
-console.log(obj);
+console.log(obj); // obj 출력
