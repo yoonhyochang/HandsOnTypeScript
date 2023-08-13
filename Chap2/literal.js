80d59146d4785324115150f75ad3f64c536b8649
gptus <yhc9308@naver.com>
2023-08-13 15:34:08 +0900

    타입 별칭

--- Chap2/literal.js
+++
@@ -1,5 +0,0 @@
-// 'literal' 변수는 "tom", "linda", "jeff", "sue" 중 하나의 값을 가져야 함
-var literal = "linda";
-literal = "sue"; // "sue"로 값을 변경 가능
-// literal = "john"; // 오류! "john"은 허용되지 않는 값
-console.log(literal); // "sue" 출력
--- Chap2/typeAlias.ts
+++ Chap2/typeAlias.ts
@@ -1,12 +1,13 @@
+// 'Points' 타입은 20, 30, 40, 50 중 하나의 값을 가져야 함
 type Points = 20 | 30 | 40 | 50;
-let score: Points = 20;
+let score: Points = 20; // 'score' 변수는 'Points' 타입이므로 위의 값 중 하나만 할당 가능
+console.log(score); // 20 출력
 
-console.log(score);
-
+// 'ComplexPerson' 타입은 여러 속성을 가진 객체를 정의
 type ComplexPerson = {
-    name: string,
-    age: number,
-    birthday: Date,
-    married: boolean,
-    address: string
+    name: string,      // 문자열 타입의 이름
+    age: number,       // 숫자 타입의 나이
+    birthday: Date,    // 날짜 타입의 생일
+    married: boolean,  // 부울 타입의 결혼 여부
+    address: string    // 문자열 타입의 주소
 }
