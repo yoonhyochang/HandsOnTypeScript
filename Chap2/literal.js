83b426f441608f4ffa5820a33c77d5d492607fc1
gptus <yhc9308@naver.com>
2023-08-13 15:28:45 +0900

    리터럴 타입

--- Chap2/literal.js
+++ Chap2/literal.js
@@ -1,5 +1,5 @@
 // 'literal' 변수는 "tom", "linda", "jeff", "sue" 중 하나의 값을 가져야 함
-let literal: "tom" | "linda" | "jeff" | "sue" = "linda";
+var literal = "linda";
 literal = "sue"; // "sue"로 값을 변경 가능
 // literal = "john"; // 오류! "john"은 허용되지 않는 값
 console.log(literal); // "sue" 출력
