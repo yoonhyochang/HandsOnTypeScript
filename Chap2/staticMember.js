200199f0966095a1c2bbde2565b4bdcad686a53c
gptus <yhc9308@naver.com>
2023-08-13 22:47:40 +0900

    정적 속성과 메서드

--- Chap2/staticMember.js
+++ Chap2/staticMember.js
@@ -1,24 +1,26 @@
 // class ClassA {
-//     static typeName: string;
+//     static typeName: string; // 정적 멤버 변수 선언
 //     constructor() {
+//         // 생성자 함수
 //     }
 //     static getFullName() {
-//         return "ClassA " + ClassA.typeName;
+//         return "ClassA " + ClassA.typeName; // 정적 메서드, typeName 변수를 사용
 //     }
 // }
-// const a = new ClassA();
-// console.log(ClassA.typeName);
-var Runner = /** @class */ (function () {
-    function Runner(typeName) {
+// const a = new ClassA(); // ClassA 인스턴스 생성
+// console.log(ClassA.typeName); // 정적 멤버 변수 typeName 출력, undefined 반환
+// // console.log(a.typeName); // 정적 멤버 변수 typeName 출력, 오류 발생
+// //정적 멤버는 클래스 레벨에서 정의되므로 인스턴스 레벨에서는 접근할 수 없습니다
+class Runner {
+    constructor(typeName) {
         this.typeName = typeName;
+    } // 생성자로 typeName 멤버 변수 초기화
+    run() {
+        Runner.lastRunTypeName = this.typeName; // run 메서드 호출 시, 정적 변수에 현재 인스턴스의 typeName 값 할당
     }
-    Runner.prototype.run = function () {
-        Runner.lastRunTypeName = this.typeName;
-    };
-    return Runner;
-}());
-var a = new Runner("a");
-var b = new Runner("b");
-b.run();
-a.run();
-console.log(Runner.lastRunTypeName);
+}
+const b = new Runner("b"); // typeName을 "b"로 가진 Runner 인스턴스 생성
+const a = new Runner("a"); // typeName을 "a"로 가진 Runner 인스턴스 생성
+b.run(); // b의 run 메서드 호출, lastRunTypeName은 "b"가 됨
+a.run(); // a의 run 메서드 호출, lastRunTypeName은 "a"가 됨
+console.log(Runner.lastRunTypeName); // "a" 출력, 마지막으로 실행된 인스턴스의 typeName
--- Chap2/staticMember.ts
+++ Chap2/staticMember.ts
@@ -1,33 +1,41 @@
 // class ClassA {
-//     static typeName: string;
+//     static typeName: string; // 정적 멤버 변수 선언
 
 //     constructor() {
-        
+//         // 생성자 함수
 //     }
     
 //     static getFullName() {
-//         return "ClassA " + ClassA.typeName;
+//         return "ClassA " + ClassA.typeName; // 정적 메서드, typeName 변수를 사용
 //     }
 // }
 
-// const a = new ClassA();
-// console.log(ClassA.typeName);
+// const a = new ClassA(); // ClassA 인스턴스 생성
+// console.log(ClassA.typeName); // 정적 멤버 변수 typeName 출력, undefined 반환
+// // console.log(a.typeName); // 정적 멤버 변수 typeName 출력, 오류 발생
+// //정적 멤버는 클래스 레벨에서 정의되므로 인스턴스 레벨에서는 접근할 수 없습니다
 
 
-class Runner {    
-    static lastRunTypeName: string;
 
-    constructor(private typeName: string) {}
+
+
+
+class Runner {
+    static lastRunTypeName: string; // 클래스 레벨에서 공유되는 정적 변수
+
+    constructor(private typeName: string) {} // 생성자로 typeName 멤버 변수 초기화
     
     run() {        
-        Runner.lastRunTypeName = this.typeName;
+        Runner.lastRunTypeName = this.typeName; // run 메서드 호출 시, 정적 변수에 현재 인스턴스의 typeName 값 할당
     }
 }
 
-const a = new Runner("a");
-const b = new Runner("b");
+const b = new Runner("b"); // typeName을 "b"로 가진 Runner 인스턴스 생성
+const a = new Runner("a"); // typeName을 "a"로 가진 Runner 인스턴스 생성
 
-b.run();
-a.run();
+b.run(); // b의 run 메서드 호출, lastRunTypeName은 "b"가 됨
+a.run(); // a의 run 메서드 호출, lastRunTypeName은 "a"가 됨
 
-console.log(Runner.lastRunTypeName);
+console.log(Runner.lastRunTypeName); // "a" 출력, 마지막으로 실행된 인스턴스의 typeName
+
+    
