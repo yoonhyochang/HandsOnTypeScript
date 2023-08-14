80b928f706219fd58878ac7e85a6fe2ccd7ce570
gptus <yhc9308@naver.com>
2023-08-14 14:32:48 +0900

    최신기능과 컴파일러 설정-널병합

--- Chap2/nullCoalescing.js
+++ Chap2/nullCoalescing.js
@@ -1,4 +1,5 @@
-var val1 = 2;
+// val1은 undefined이므로 nullish 병합 연산자(??)가 val2의 값인 10을 반환합니다.
+var val1 = undefined;
 var val2 = 10;
-var result = (val1 !== null && val1 !== void 0 ? val1 : val2);
-console.log(result);
+var result = val1 !== null && val1 !== void 0 ? val1 : val2; // result의 값은 10입니다.
+console.log(result); // 10을 출력합니다.
--- Chap2/nullCoalescing.ts
+++ Chap2/nullCoalescing.ts
@@ -1,4 +1,5 @@
+// val1은 undefined이므로 nullish 병합 연산자(??)가 val2의 값인 10을 반환합니다.
 const val1 = undefined;
 const val2 = 10;
-const result = val1 ?? val2;
-console.log(result);
+const result = val1 ?? val2; // result의 값은 10입니다.
+console.log(result); // 10을 출력합니다.
