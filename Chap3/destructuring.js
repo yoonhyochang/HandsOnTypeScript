1964bfef88774bf447b680dff9a78ccd754a4406
gptus <yhc9308@naver.com>
2023-08-14 18:39:29 +0900

    구조분해

--- Chap3/destructuring.js
+++ Chap3/destructuring.js
@@ -1,5 +1,5 @@
 function getEmployee(id) {
-
+    // 주어진 ID에 해당하는 직원 정보 반환
     return {
         name: "John",
         age: 35,
@@ -8,12 +8,12 @@ function getEmployee(id) {
     }
 }
 
-const { name: fullName, age } = getEmployee(22);
-console.log("employee", fullName, age);
+const { name: fullName, age } = getEmployee(22); // 직원 정보에서 이름과 나이 추출
+console.log("employee", fullName, age); // 직원의 이름과 나이 출력
 
 
 function getEmployeeWorkInfo(id) {
-
+    // 주어진 ID에 해당하는 직원의 업무 관련 정보 반환
     return [
         id,
         "Office St",
@@ -21,5 +21,6 @@ function getEmployeeWorkInfo(id) {
     ]
 }
 
-const [id, officeAddress] = getEmployeeWorkInfo(33);
-console.log("employee", id, officeAddress);
+const [id, officeAddress] = getEmployeeWorkInfo(33); // 직원의 업무 관련 정보에서 ID와 사무실 주소 추출
+
+console.log("employee", id, officeAddress); // 직원의 ID와 사무실 주소 출력
