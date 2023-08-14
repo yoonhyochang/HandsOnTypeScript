61bc2b1f098fe287086f32f5a5713c2772ae7090
gptus <yhc9308@naver.com>
2023-08-14 18:45:56 +0900

    레스트

--- Chap3/rest.js
+++ Chap3/rest.js
@@ -1,5 +1,6 @@
 function doSomething(a, ...others) {
-    console.log(a, others, others[others.length - 1]);
+    // 첫 번째 인수는 'a'에, 나머지 인수들은 'others' 배열에 저장
+    console.log(a, others, others[others.length - 1]); // 'a', 'others' 배열, 'others' 배열의 마지막 요소 출력
 }
 
-doSomething(1,2,3,4,5,6,7);
+doSomething(1,2,3,4,5,6,7); // 1은 'a'로, 나머지 숫자들은 'others' 배열로 전달
