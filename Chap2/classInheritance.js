0d58e7326ff46b321ef5b37d8757668df0611488
gptus <yhc9308@naver.com>
2023-08-14 12:24:20 +0900

    상속 이해하기

--- Chap2/classInheritance.js
+++ Chap2/classInheritance.js
@@ -2,10 +2,12 @@ var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
         return extendStatics(d, b);
     };
     return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
         extendStatics(d, b);
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
@@ -14,9 +16,9 @@ var __extends = (this && this.__extends) || (function () {
 var Vehicle = /** @class */ (function () {
     function Vehicle(wheelCount) {
         this.wheelCount = wheelCount;
-    }
+    } // 부모 클래스에서 wheelCount를 설정하는 생성자
     Vehicle.prototype.showNumberOfWheels = function () {
-        console.log("moved " + this.wheelCount + " miles");
+        console.log("moved ".concat(this.wheelCount, " miles")); // wheelCount를 출력하는 메서드
     };
     return Vehicle;
 }());
@@ -35,6 +37,6 @@ var Automobile = /** @class */ (function (_super) {
     return Automobile;
 }(Vehicle));
 var motorCycle = new Motorcycle();
-motorCycle.showNumberOfWheels();
+motorCycle.showNumberOfWheels(); // "moved 2 miles"를 출력
 var autoMobile = new Automobile();
-autoMobile.showNumberOfWheels();
+autoMobile.showNumberOfWheels(); // "moved 4 miles"를 출력
--- Chap2/classInheritance.ts
+++ Chap2/classInheritance.ts
@@ -1,28 +1,27 @@
 class Vehicle {
-    constructor(protected wheelCount: number) {}
+    constructor(protected wheelCount: number) {} // 부모 클래스에서 wheelCount를 설정하는 생성자
 
     showNumberOfWheels() {
-        console.log(`moved ${this.wheelCount} miles`);
+        console.log(`moved ${this.wheelCount} miles`); // wheelCount를 출력하는 메서드
     }
 }
 
-class Motorcycle extends Vehicle {
+class Motorcycle extends Vehicle { // Motorcycle 클래스는 Vehicle 클래스를 상속
     constructor() {
-        super(2);
+        super(2); // 부모 클래스의 생성자를 호출하여 wheelCount를 2로 설정
     }
-    updateWheelCount(newWheelCount: number){
-        this.wheelCount = newWheelCount;
-    }
+
+
 }
 
-class Automobile extends Vehicle {
+class Automobile extends Vehicle { // Automobile 클래스도 Vehicle 클래스를 상속
     constructor() {
-        super(4);
+        super(4); // 부모 클래스의 생성자를 호출하여 wheelCount를 4로 설정
     }
 }
 
 const motorCycle = new Motorcycle();
-motorCycle.showNumberOfWheels();
+motorCycle.showNumberOfWheels(); // "moved 2 miles"를 출력
 
 const autoMobile = new Automobile();
-autoMobile.showNumberOfWheels();
+autoMobile.showNumberOfWheels(); // "moved 4 miles"를 출력
